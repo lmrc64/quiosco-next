76bf903c31d3565f08abf4bb8e47c18df85df2ce
lmrc64 <21030064@itcelaya.edu.mx>
2024-05-29 19:56:47 -0600

    Implementación de administración de productos

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,7 +1,38 @@
+//import { redirect } from 'next/navigation'
+//import ProductsPagination from "@/components/products/ProductsPagination";
+import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
+//import Link from 'next/link';
+//import ProductSearchForm from '@/components/products/ProductSearchForm';
 
-export default  function ProductsPage(){
-    return(
-        <Heading>Administrar Productos</Heading>
+async function getProducts() {
+
+    const products = await prisma.product.findMany({
+
+        include: {
+            category: true
+        }
+    })
+    return products
+}
+
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
+
+export default async function ProductsPage() {
+
+    const products = await getProducts()
+
+
+    return (
+        <>
+            <Heading>Administrar Productos</Heading>
+
+            <ProductTable
+                products={products}
+            />
+
+
+        </>
     )
 }
